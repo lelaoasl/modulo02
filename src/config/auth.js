eb05e6e4fac2a4f11512cf5dd75cb9f58d4d9db2
Leandro <lelaoasl@gmail.com>
2019-10-18 02:00:54 -0300

    alala

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '90586999304a743f3074b28bb0b7311d',
+  expiresIn: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '348fa8cde0485c',
+    pass: '8f43c860f542d7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
